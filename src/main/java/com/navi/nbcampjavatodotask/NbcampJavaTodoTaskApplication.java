8efa66e0f433ad621e045414e6e4886b77b5d3b0
tsdnhts <jyjangtop@hanmail.net>
2024-07-27 22:59:15 +0900

    initial commit

---
+++ src/main/java/com/navi/nbcampjavatodotask/NbcampJavaTodoTaskApplication.java
@@ -0,0 +1,13 @@
+package com.navi.nbcampjavatodotask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbcampJavaTodoTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NbcampJavaTodoTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/navi/nbcampjavatodotask/NbcampJavaTodoTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.navi.nbcampjavatodotask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbcampJavaTodoTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
